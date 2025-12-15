61f1998b2169d936465efabafee965ffa6d098f3
triumphkay <triumphkay@gmail.com>
2025-12-15 15:54:14 +0900

    fix: remove target device - ipad

--- app/app.config.js
+++ app/app.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     assetBundlePatterns: ["**/*"],
     ios: {
-      supportsTablet: true,
+      supportsTablet: false,
       bundleIdentifier: "com.triumphkay.historyk",
       buildNumber: String(versionCode),
       infoPlist: {
